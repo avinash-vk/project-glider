9184d4c673954576b49fd3297cc308a84e690e06
Srujan <srujan.deshpande@gmail.com>
2020-07-03 10:36:39 +0530

    Basic bold working

--- markdown.js
+++ markdown.js
@@ -3,7 +3,34 @@ $(function(){
 $( "#textform" ).submit(function( event ) {
   event.preventDefault();
   var $rawtext = $("#textbody").val();
-  $("#mdbody").val($rawtext);
+  var $newtext = "";
+
+  $("#mdbody").html("");
+  var boldstate = false;
+
+  for (let i = 0; i < $rawtext.length; i++) {
+
+    if($rawtext[i] == '*' && i!=0 && $rawtext[i-1]=='*' && boldstate == false){
+      $newtext+="<b>";
+      boldstate = true;
+      continue;
+    }
+    else if($rawtext[i] == '*' && i!=0 && $rawtext[i-1]=='*' && boldstate == true){
+      $newtext+="</b>";
+      boldstate = false;
+      continue;
+    }
+    else{
+      $newtext+=$rawtext[i];
+    }
+  }
+
+  $('#mdbody').html($newtext);
+
+
+
+
+
 });
 
 });
